2acad5c0f52b84b75a1afab5e5f1302559511129
Tengiz Machavariani <tezimach@yahoo.com>
2022-12-12 15:44:33 +0400

    fixed

--- frontend/utils/const.js
+++ frontend/utils/const.js
@@ -4,6 +4,6 @@ import ticktock from './ticktock_Clone.json'
 export const SOLANA_HOST = clusterApiUrl('devnet')
 
 export const TICKTOCK_PROGRAM_ID = new PublicKey(
-    "14sBpgJSmzMo5DzJESGZqdvNyaRTA1uSvBt4at1tEJm2"
+    "3CcDPmq5vBUABsCNx2frFGoXUnYtq3cGUVGwp5qHjU85"
 )
 export const TICKTOCK_IDL = ticktock
